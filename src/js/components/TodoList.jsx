114378a5c1be0a4686f475260a38ee2a07bf30ba
juanpass <34844456+juanpass@users.noreply.github.com>
2025-03-27 04:12:49 +0000

    se agrega mensaje no hay tareas

--- src/js/components/TodoList.jsx
+++ src/js/components/TodoList.jsx
@@ -2,17 +2,21 @@ import React from "react";
 import TodoItem from "./TodoItem";
 
 const TodoList = ({ todos, onDelete }) => {
-	return (
-		<ul className="todo-list">
-			{todos.map((todo, index) => (
-				<TodoItem
-					key={index}
-					text={todo.text}
-					onDelete={() => onDelete(index)}
-				/>
-			))}
-		</ul>
-	);
+    return (
+        <ul className="todo-list">
+            {todos.length === 0 ? (
+                <li className="empty-message">No hay tareas, añadir tareas</li>
+            ) : (
+                todos.map((todo, index) => (
+                    <TodoItem
+                        key={index}
+                        text={todo.text}
+                        onDelete={() => onDelete(index)}
+                    />
+                ))
+            )}
+        </ul>
+    );
 };
 
 export default TodoList;
