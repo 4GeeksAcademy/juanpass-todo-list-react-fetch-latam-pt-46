0ba30b6efd53b174654ecc4fa5fa9508599837f9
juanpass <34844456+juanpass@users.noreply.github.com>
2025-03-29 02:25:41 +0000

    se corrige hover icono eliminar

--- src/js/components/TodoItem.jsx
+++ src/js/components/TodoItem.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 const TodoItem = ({ text, onDelete }) => {
 	return (
+		<div>
 		<li className="todo-item">
 			<span>{text}</span>
 			<button className="delete-btn" onClick={onDelete}>
 				x
 			</button>
 		</li>
+			</div>
 	);
 };
 
